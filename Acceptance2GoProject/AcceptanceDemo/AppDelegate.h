da601c85eba4f32bafac61617f7f1c18354b64fe
Kenneth <de_poon@hotmail.com>
2014-03-06 01:29:36 +0800

    Setup Boiler plate

---
+++ Acceptance2GoProject/AcceptanceDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AcceptanceDemo
+//
+//  Created by Kenneth Poon on 2/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
